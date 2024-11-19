f3615b59c62f391ac46032e2c2cd9978d2ce3d92
AsumaToki04 <megamanzxu@hotmail.com>
2024-11-18 19:20:55 -0500

    Initial Commit

---
+++ Sesion2d/Sesion2dApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sesion2dApp.swift
+//  Sesion2d
+//
+//  Created by DAMII on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sesion2dApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
